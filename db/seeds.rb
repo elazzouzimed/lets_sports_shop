b70a0b4e20432d5763c9115167c53389e4147a67
karim chlih <linasanet@gmail.com>
2022-03-23 15:07:45 +0100

    seed Done

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ p "creating sports"
 football = Sport.create({ name: "Football", description: "sport qui se joue avec un ballon ovale, sur un terrain de 90 m sur 52 m, divisé en tranches de 4,50 m, entre deux équipes de onze ou douze joueurs utilisant les mains et les pieds."})
 tennis = Sport.create({ name: "tennis", description: "Le tennis est un sport de raquette qui oppose soit deux joueurs (on parle alors de jeu en simple) soit quatre joueurs qui forment deux équipes de deux (on parle alors de jeu en double.)"})
 golf = Sport.create({ name: "golf", description: "Le golf est un sport de précision se jouant en plein air, qui consiste à envoyer une balle dans un trou à l'aide de clubs. Le but du jeu consiste à effectuer, sur un parcours défini, le moins de coups possible. Précision, endurance, technicité, concentration sont des qualités primordiales pour cette activité."})
-
+basketball = Sport.create({ name: "basketball", description: "Le basket-ball ou basket ou Basketball (selon les rectifications orthographiques du français en 1990 1), est un sport collectif qui se joue entre deux équipes de cinq joueurs"})
 p "creating Items"
 
 Item.create({ user_id: user1.id, sport_id: football.id, description: "c'est un ballon d’origin signe adidas model 2022", name:"ballon adidas", price: 125})
