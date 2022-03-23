a0f4c0d1b02e0f0f2fea629fdb58c83a8334618b
Hamza hanfi <hanfihamza20@gmail.com>
2022-03-23 14:38:02 +0100

    DB fixed

--- db/migrate/20220321164435_create_items.rb
+++ db/migrate/20220321164435_create_items.rb
@@ -4,8 +4,8 @@ class CreateItems < ActiveRecord::Migration[6.1]
       t.string :name
       t.string :description
       t.float :price
+      t.integer :seller_id
       t.references :sport, null: false, foreign_key: true
-      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
