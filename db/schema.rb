b3e1e97a02c30f2c0779a14edfdfad0d8b23211a
Hamza hanfi <hanfihamza20@gmail.com>
2022-03-23 14:36:28 +0100

    cloudinary Setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_22_111349) do
+ActiveRecord::Schema.define(version: 2022_03_23_111945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
   create_table "feedbacks", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -29,12 +57,11 @@ ActiveRecord::Schema.define(version: 2022_03_22_111349) do
     t.string "name"
     t.string "description"
     t.float "price"
+    t.integer "seller_id"
     t.bigint "sport_id", null: false
-    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["sport_id"], name: "index_items_on_sport_id"
-    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "sports", force: :cascade do |t|
@@ -45,14 +72,13 @@ ActiveRecord::Schema.define(version: 2022_03_22_111349) do
   end
 
   create_table "transactions", force: :cascade do |t|
-    t.bigint "user_id", null: false
+    t.integer "buyer_id"
     t.string "status"
     t.bigint "item_id", null: false
     t.float "price_offer"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["item_id"], name: "index_transactions_on_item_id"
-    t.index ["user_id"], name: "index_transactions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -70,10 +96,12 @@ ActiveRecord::Schema.define(version: 2022_03_22_111349) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "feedbacks", "users"
   add_foreign_key "feedbacks", "users", column: "comentator_id"
   add_foreign_key "items", "sports"
-  add_foreign_key "items", "users"
+  add_foreign_key "items", "users", column: "seller_id"
   add_foreign_key "transactions", "items"
-  add_foreign_key "transactions", "users"
+  add_foreign_key "transactions", "users", column: "buyer_id"
 end
