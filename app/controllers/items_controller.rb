cf7f6048f0ff1a6c03046200459c5f3d8c49e1bd
karim chlih <linasanet@gmail.com>
2022-03-25 12:13:08 +0100

    add new item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,7 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
+    @item.user = current_user
     if @item.save
       redirect_to item_path(@item)
     else
