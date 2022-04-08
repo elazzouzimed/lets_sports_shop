27c50452efb1e1515257223fa5979c3af96b0c8a
karim chlih <linasanet@gmail.com>
2022-04-08 12:51:19 +0000

    fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :users
-
-
   resources :sports do
     resources :items
   end
+  resources :items, only: [:new,:create]
 
-
-  resources :sports
   resources :transactions
   resources :feedbacks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
